da7fac8e43415e08eb0c1e96a8ca2bd188fbcc9f
Филип Арсовски <arshetamine@protonmail.com>
2021-05-08 20:28:19 +0200

    Redo all time stats.

--- backend/database/initial.php
+++ backend/database/initial.php
@@ -1,21 +1,12 @@
 <?php
 require_once '../config.php';
 require_once 'writeCountries.php';
-require_once 'writeAllTime.php';
 
 $createCountriesTable = "CREATE TABLE IF NOT EXISTS `covid-tracker`.`countries`(
     slug VARCHAR( 128 ) PRIMARY KEY,
     country VARCHAR( 256 ) NOT NULL,
     iso2 VARCHAR( 32 ) NOT NULL);";
 
-$createAllTimeTable = "CREATE TABLE IF NOT EXISTS `covid-tracker`.`all_time`(
-    slug VARCHAR( 128 ) PRIMARY KEY,
-    total_confirmed INT( 128 ) NOT NULL,
-    total_deaths INT( 128 ) NOT NULL,
-    total_recovered INT( 128 ) NOT NULL);";
-
 $conn->exec($createCountriesTable);
-$conn->exec($createAllTimeTable);
 
 writeCountries($conn);
-writeAllTimeStats($conn);
