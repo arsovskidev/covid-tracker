8904263cdb3e9830391f80b5244377bcdbd9c837
Филип Арсовски <arshetamine@protonmail.com>
2021-05-08 20:12:42 +0200

    Creating table for all time stats if not there.

--- backend/database/initial.php
+++ backend/database/initial.php
@@ -8,7 +8,14 @@ $createCountriesTable = "CREATE TABLE IF NOT EXISTS `covid-tracker`.`countries`(
     country VARCHAR( 256 ) NOT NULL,
     iso2 VARCHAR( 32 ) NOT NULL);";
 
+$createAllTimeTable = "CREATE TABLE IF NOT EXISTS `covid-tracker`.`all_time`(
+    slug VARCHAR( 128 ) PRIMARY KEY,
+    total_confirmed INT( 128 ) NOT NULL,
+    total_deaths INT( 128 ) NOT NULL,
+    total_recovered INT( 128 ) NOT NULL);";
+
 $conn->exec($createCountriesTable);
+$conn->exec($createAllTimeTable);
 
 writeCountries($conn);
 writeAllTimeStats($conn);
